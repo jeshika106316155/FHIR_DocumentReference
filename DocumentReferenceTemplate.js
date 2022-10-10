601f870cde0b091e6711ef6501d730e61407e7f5
unknown <forimportjesh@gmail.com>
2022-10-10 15:48:14 +0800

    post fhir docref

---
+++ DocumentReferenceTemplate.js
@@ -0,0 +1,30 @@
+var docRefTemp = {
+    "resourceType": "DocumentReference",
+    "status": "current",
+    "subject": {
+        "reference": "Patient/46179774-1a1e-4ae4-b626-9ff53fa080f4",
+        "display": "Will Smith"
+    },
+    "date": "2022-10-05T12:24:39",
+    "author": [
+        {
+            "reference": "Patient/46179774-1a1e-4ae4-b626-9ff53fa080f4",
+            "display": "Will Smith"
+        }
+    ],
+    "authenticator": {
+        "reference": "Organization/MIPatientPortal",
+        "display": "MI Patient Portal"
+    },
+    "custodian": {
+        "reference": "Organization/MIPatientPortal",
+        "display": "MI Patient Portal"
+    },
+    "content": [ 
+        {
+            "attachment": {
+                "url": ""
+            }
+        }
+    ]
+}
---
+++ setting.js
@@ -0,0 +1,1 @@
+var FHIRServer ="https://203.64.84.150:58443/portaltest1/fhir/";
