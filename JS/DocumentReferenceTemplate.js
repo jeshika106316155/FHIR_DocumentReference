9db55e9049ce09d816dbbdec78816eb9736f8c84
106316155@gms.tcu.edu.tw <106316155@gms.tcu.edu.tw>
2023-01-04 00:28:28 +0800

    230103 last

--- JS/DocumentReferenceTemplate.js
+++ JS/DocumentReferenceTemplate.js
@@ -6,23 +6,16 @@ var docRefTemp = {
      }],
     "status": "current",
     "subject": {
-        "reference": "Patient/88df8172-9f58-4a0f-bcca-bcdcd5ea6485",
-        "display": "Will Smith"
+        "reference": "Patient/88df8172-9f58-4a0f-bcca-bcdcd5ea6485"
     },
     "date": "2022-10-05T12:24:39",
     "author": [
         {
-            "reference": "PractitionerRole/af055cb6-bd06-4ce4-9967-90d4e9f25044",
-            "display": "Will Smith"
+            "reference": "PractitionerRole/af055cb6-bd06-4ce4-9967-90d4e9f25044"
         }
     ],
-    "authenticator": {
-        "reference": "Organization/98a6f5c5-af58-41b5-bd6f-d8ca996b5580",
-        "display": "MI Patient Portal"
-    },
     "custodian": {
-        "reference": "Organization/98a6f5c5-af58-41b5-bd6f-d8ca996b5580",
-        "display": "MI Patient Portal"
+        "reference": "Organization/98a6f5c5-af58-41b5-bd6f-d8ca996b5580"
     },
     "content": [ 
         
