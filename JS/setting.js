ebeed4c40f03e2c5f6ba2bad8fbaab00646b981f
106316155@gms.tcu.edu.tw <106316155@gms.tcu.edu.tw>
2023-01-04 00:30:55 +0800

    230103 change https

--- JS/setting.js
+++ JS/setting.js
@@ -1,2 +1,2 @@
 /** FHIR Server url */
-var FHIRServer = "http://hapi.fhir.org/baseR4/";//"https://203.64.84.150:58443/r5/fhir/"//"https://203.64.84.150:58443/portaltest1/fhir/";
+var FHIRServer = "https://hapi.fhir.org/baseR4/";//"https://203.64.84.150:58443/r5/fhir/"//"https://203.64.84.150:58443/portaltest1/fhir/";
